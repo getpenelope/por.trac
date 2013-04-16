ccdef792e6293bf2c41f7d18684c217e4465cfc0
Andrew Mleczko <andrew@mleczko.net>
2013-04-16 17:43:21 +0200

    Preparing release 1.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='por.trac',
-      version='1.2.1.2.dev0',
+      version='1.3',
       description='Penelope: Trac integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[

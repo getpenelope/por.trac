085382b57ba5d34a46f98a85dd42774c847f6ec0
Andrew Mleczko <andrew@mleczko.net>
2013-04-16 17:50:31 +0200

    revert

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
     ]
 
 setup(name='por.trac',
-      version='1.4.dev0',
+      version='1.3.dev0',
       description='Penelope: Trac integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
